46ec7589ae9d53066aa6cb710e22b32597ff9f7e
もっくん <mokkunpc@gmail.com>
2025-05-23 13:12:24 +0900

    feat: hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import styles from "./style.module.css";
 import Header from "./components/header";
+import Hero from "./components/hero";
 
 export default function App() {
   return (
     <div className={styles.container}>
       {/* ヘッダー */}
       <Header />
+
+      {/* ヒーロー */}
+      <Hero />
     </div>
   );
 }
---
+++ src/components/hero/index.tsx
@@ -0,0 +1,27 @@
+import styles from "./style.module.css";
+
+export default function Hero() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <img
+          src="/images/icon.jpg"
+          alt="もっくんのアイコン"
+          className={styles.icon}
+        />
+        <h1 className={styles.title}>
+          こんにちは！
+          <span style={{ color: "var(--text-title)" }}>もっくん</span>
+          です！
+        </h1>
+        <p className={styles.description}>
+          ガジェット・アニメ・麻雀・かわいいものが好きです。
+          <br />
+          現在近大高専(5年)で情シス・プログラミングを勉強中… &
+          学生寮で寮長やってます。
+        </p>
+      </div>
+      {/* TODO スクロールを促す矢印アイコン */}
+    </div>
+  );
+}
